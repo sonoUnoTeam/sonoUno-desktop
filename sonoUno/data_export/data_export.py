ed032fd08571418ffb356637c180c437750f0611
Johanna Casado <johi.ceh@gmail.com>
2022-02-11 11:19:37 -0300

    state variable
    I add a state variable with the default option as true, to create or not
    the log files.

--- sonoUno/data_export/data_export.py
+++ sonoUno/data_export/data_export.py
@@ -20,69 +20,30 @@ import pandas
 class DataExport(object):
 
 
-    def __init__(self):
+    def __init__(self, log=True):
 
         """
         This class allow to export data and save the outputs of the software.
         First save the time and then create the error and output file.
         """
-        # Save the home directory to append to output folder
-        homepath = str(Path.home())
-        # Create the string with the time to create the output file.
-        now = datetime.datetime.now()
-        time = now.strftime('%Y-%m-%d_%H-%M-%S')
-        # Create error variable to store posible errors in the save process
-        error_loggingfiles = ''
-        # Create the errors file depending on the operative system.
-        try:
-            if platform.system() == 'Windows':
-                logging.basicConfig(
-                    filename=(homepath + '\\sonouno\\output\\err_' + time 
-                        + '.log'), 
-                    filemode = 'w+', 
-                    level = logging.DEBUG
-                    )
-            elif platform.system() == 'Linux':
-                logging.basicConfig(
-                    filename = (homepath + '/sonouno/output/err_' + time 
-                        + '.log'), 
-                    filemode = 'w+', 
-                    level = logging.DEBUG
-                    )
-            elif platform.system() == 'Darwin':
-                logging.basicConfig(
-                    filename = (homepath + '/sonouno/output/err_' + time 
-                        + '.log'), 
-                    filemode = 'w+', 
-                    level = logging.DEBUG
-                    )
-            else:
-                error_loggingfiles = (time + ': The operative system is unknown and the \
-                    error file could not be created.')
-                print(error_loggingfiles)
-                
-                # I have to print this messages in the future.
-                # wx.MessageBox(
-                #     message = error,
-                #     caption = 'Error creating error file', 
-                #     style = wx.OK|wx.ICON_INFORMATION
-                #     )
-        except Exception as Error:   
-            # Checking if the problem is that the software don't find the
-            # output folder. If this is the problem, we create the folder
-            # and try again.
-            exception_error = str(Error)
-            if exception_error.find('No such file or directory') is not -1 or exception_error.find('No existe el archivo o el directorio') is not -1:
+        if log:
+            # Save the home directory to append to output folder
+            homepath = str(Path.home())
+            # Create the string with the time to create the output file.
+            now = datetime.datetime.now()
+            time = now.strftime('%Y-%m-%d_%H-%M-%S')
+            # Create error variable to store posible errors in the save process
+            error_loggingfiles = ''
+            # Create the errors file depending on the operative system.
+            try:
                 if platform.system() == 'Windows':
-                    os.makedirs(homepath + '\\sonouno\\output')
                     logging.basicConfig(
-                        filename = (homepath + '\\sonouno\\output\\err_' + time
+                        filename=(homepath + '\\sonouno\\output\\err_' + time 
                             + '.log'), 
                         filemode = 'w+', 
                         level = logging.DEBUG
                         )
                 elif platform.system() == 'Linux':
-                    os.makedirs(homepath + '/sonouno/output')
                     logging.basicConfig(
                         filename = (homepath + '/sonouno/output/err_' + time 
                             + '.log'), 
@@ -90,7 +51,6 @@ class DataExport(object):
                         level = logging.DEBUG
                         )
                 elif platform.system() == 'Darwin':
-                    os.makedirs(homepath + '/sonouno/output')
                     logging.basicConfig(
                         filename = (homepath + '/sonouno/output/err_' + time 
                             + '.log'), 
@@ -98,62 +58,103 @@ class DataExport(object):
                         level = logging.DEBUG
                         )
                 else:
-                    error_loggingfiles = (time + (': The operative system is unknown and \
-                        the error file could not be created.'))
+                    error_loggingfiles = (time + ': The operative system is unknown and the \
+                        error file could not be created.')
+                    print(error_loggingfiles)
+                    
+                    # I have to print this messages in the future.
+                    # wx.MessageBox(
+                    #     message = error,
+                    #     caption = 'Error creating error file', 
+                    #     style = wx.OK|wx.ICON_INFORMATION
+                    #     )
+            except Exception as Error:   
+                # Checking if the problem is that the software don't find the
+                # output folder. If this is the problem, we create the folder
+                # and try again.
+                exception_error = str(Error)
+                if exception_error.find('No such file or directory') is not -1 or exception_error.find('No existe el archivo o el directorio') is not -1:
+                    if platform.system() == 'Windows':
+                        os.makedirs(homepath + '\\sonouno\\output')
+                        logging.basicConfig(
+                            filename = (homepath + '\\sonouno\\output\\err_' + time
+                                + '.log'), 
+                            filemode = 'w+', 
+                            level = logging.DEBUG
+                            )
+                    elif platform.system() == 'Linux':
+                        os.makedirs(homepath + '/sonouno/output')
+                        logging.basicConfig(
+                            filename = (homepath + '/sonouno/output/err_' + time 
+                                + '.log'), 
+                            filemode = 'w+', 
+                            level = logging.DEBUG
+                            )
+                    elif platform.system() == 'Darwin':
+                        os.makedirs(homepath + '/sonouno/output')
+                        logging.basicConfig(
+                            filename = (homepath + '/sonouno/output/err_' + time 
+                                + '.log'), 
+                            filemode = 'w+', 
+                            level = logging.DEBUG
+                            )
+                    else:
+                        error_loggingfiles = (time + (': The operative system is unknown and \
+                            the error file could not be created.'))
+                        print(error_loggingfiles)
+                        # wx.MessageBox(
+                        #     message = error,
+                        #     caption = 'Error creating error file', 
+                        #     style = wx.OK|wx.ICON_INFORMATION
+                        #     )
+                else:
+                    error_loggingfiles = (time + ': Error generating the error file. The error\
+                        message is: \n' + exception_error)
                     print(error_loggingfiles)
                     # wx.MessageBox(
                     #     message = error,
                     #     caption = 'Error creating error file', 
                     #     style = wx.OK|wx.ICON_INFORMATION
                     #     )
-            else:
-                error_loggingfiles = (time + ': Error generating the error file. The error\
-                    message is: \n' + exception_error)
+            try:
+                # Create the output file depending on the operative system.
+                if platform.system() == 'Windows':
+                    sys.stdout = open(
+                        file = (homepath + '\\sonouno\\output\\out_' + time 
+                            + '.log'), 
+                        mode = 'w+'
+                        )
+                elif platform.system() == 'Linux':
+                    sys.stdout = open(
+                        file = (homepath + '/sonouno/output/out_' + time 
+                            + '.log'), 
+                        mode = 'w+'
+                        )
+                elif platform.system() == 'Darwin':
+                    sys.stdout = open(
+                        file = (homepath + '/sonouno/output/out_' + time 
+                            + '.log'), 
+                        mode = 'w+'
+                        )
+                else:
+                    error_loggingfiles = (time + ': The operative system is unknown,the \
+                        output file could not be created.')
+                    print(error_loggingfiles)
+                    # wx.MessageBox(
+                    #     message = error,
+                    #     caption = 'Error creating error file', 
+                    #     style = wx.OK|wx.ICON_INFORMATION
+                    #     )
+            except Exception as Error:   
+                # Catch the problem and print it.
+                error_loggingfiles = (time + ': Error generating the output file. The error\
+                    message is: \n' + str(Error))
                 print(error_loggingfiles)
                 # wx.MessageBox(
                 #     message = error,
-                #     caption = 'Error creating error file', 
+                #     caption = 'Error creating output file', 
                 #     style = wx.OK|wx.ICON_INFORMATION
                 #     )
-        try:
-            # Create the output file depending on the operative system.
-            if platform.system() == 'Windows':
-                sys.stdout = open(
-                    file = (homepath + '\\sonouno\\output\\out_' + time 
-                        + '.log'), 
-                    mode = 'w+'
-                    )
-            elif platform.system() == 'Linux':
-                sys.stdout = open(
-                    file = (homepath + '/sonouno/output/out_' + time 
-                        + '.log'), 
-                    mode = 'w+'
-                    )
-            elif platform.system() == 'Darwin':
-                sys.stdout = open(
-                    file = (homepath + '/sonouno/output/out_' + time 
-                        + '.log'), 
-                    mode = 'w+'
-                    )
-            else:
-                error_loggingfiles = (time + ': The operative system is unknown,the \
-                    output file could not be created.')
-                print(error_loggingfiles)
-                # wx.MessageBox(
-                #     message = error,
-                #     caption = 'Error creating error file', 
-                #     style = wx.OK|wx.ICON_INFORMATION
-                #     )
-        except Exception as Error:   
-            # Catch the problem and print it.
-            error_loggingfiles = (time + ': Error generating the output file. The error\
-                message is: \n' + str(Error))
-            print(error_loggingfiles)
-            # wx.MessageBox(
-            #     message = error,
-            #     caption = 'Error creating output file', 
-            #     style = wx.OK|wx.ICON_INFORMATION
-            #     )
     
     def writeinfo(self, info):
         
