fd9ed74b3d3f6d18cd36b1151eb368e17a0f64ce
Johanna Casado <johi.ceh@gmail.com>
2022-03-22 16:40:04 -0300

    test-time
    I try to test the time to sonify specific amount of data

--- sonoUno/sonify_bash.py
+++ sonoUno/sonify_bash.py
@@ -9,6 +9,7 @@ import os
 import argparse
 import glob
 import numpy as np
+import datetime
 from data_export.data_export import DataExport
 from data_import.data_import import DataImport
 from sound_module.simple_sound import simpleSound
@@ -47,22 +48,62 @@ extension = '*.' + ext
 # Initialize a counter to show a message during each loop
 i = 1
 # Loop to walk the directory and sonify each data file
-for filename in glob.glob(os.path.join(path, extension)):
-    print("Converting data file number "+str(i)+" to sound.")
-    # Open each file
-    data, status, msg = _dataimport.set_arrayfromfile(filename, ext)
-    # Convert into numpy, split in x and y and normalyze
-    if data.shape[1]<2:
-        print("Error reading file, only detect one column.")
-        exit()
-    data = data.iloc[1:, :]
-    x = data.loc[1:, 0]
-    xnumpy = x.values.astype(np.float64)
-    y = data.loc[1:, 1]
-    ynumpy = y.values.astype(np.float64)
-    x, y, status = _math.normalize(xnumpy, ynumpy)
-    # Save the sound
-    wav_name = path + '\\' + os.path.basename(filename) + '_sound.wav'
-    _simplesound.save_sound(wav_name, x, y)
-    i = i + 1
+# now = datetime.datetime.now()
+# print(now.strftime('%Y-%m-%d_%H-%M-%S'))
+# for filename in glob.glob(os.path.join(path, extension)):
+#     print("Converting data file number "+str(i)+" to sound.")
+#     # Open each file
+#     data, status, msg = _dataimport.set_arrayfromfile(filename, ext)
+#     # Convert into numpy, split in x and y and normalyze
+#     if data.shape[1]<2:
+#         print("Error reading file, only detect one column.")
+#         exit()
+#     data = data.iloc[1:, :]
+#     x = data.loc[1:, 0]
+#     xnumpy = x.values.astype(np.float64)
+#     y = data.loc[1:, 1]
+#     ynumpy = y.values.astype(np.float64)
+#     x, y, status = _math.normalize(xnumpy, ynumpy)
+#     # Save the sound
+#     wav_name = path + '\\' + os.path.basename(filename) + '_sound.wav'
+#     _simplesound.save_sound(wav_name, x, y)
+#     now = datetime.datetime.now()
+#     print(now.strftime('%Y-%m-%d_%H-%M-%S'))
+#     i = i + 1
 
+
+now = datetime.datetime.now()
+print(now.strftime('%Y-%m-%d_%H-%M-%S'))
+n = np.arange(0, 10000, 1, dtype=int)
+wav_name = path + '\\_sound10000.wav'
+_simplesound.save_sound(wav_name, n, n)
+now = datetime.datetime.now()
+print(now.strftime('%Y-%m-%d_%H-%M-%S'))
+n = np.arange(0, 20000, 1, dtype=int)
+wav_name = path + '\\_sound20000.wav'
+_simplesound.save_sound(wav_name, n, n)
+now = datetime.datetime.now()
+print(now.strftime('%Y-%m-%d_%H-%M-%S'))
+n = np.arange(0, 30000, 1, dtype=int)
+wav_name = path + '\\_sound30000.wav'
+_simplesound.save_sound(wav_name, n, n)
+now = datetime.datetime.now()
+print(now.strftime('%Y-%m-%d_%H-%M-%S'))
+n = np.arange(0, 40000, 1, dtype=int)
+wav_name = path + '\\_sound40000.wav'
+_simplesound.save_sound(wav_name, n, n)
+now = datetime.datetime.now()
+print(now.strftime('%Y-%m-%d_%H-%M-%S'))
+
+
+
+# n = np.arange(0, 100000, 1, dtype=int)
+# wav_name = path + '\\_sound10000.wav'
+# _simplesound.save_sound(wav_name, n, n)
+# now = datetime.datetime.now()
+# print(now.strftime('%Y-%m-%d_%H-%M-%S'))
+# n = np.arange(0, 1000000, 1, dtype=int)
+# wav_name = path + '\\_sound10000.wav'
+# _simplesound.save_sound(wav_name, n, n)
+# now = datetime.datetime.now()
+# print(now.strftime('%Y-%m-%d_%H-%M-%S'))
