6d07c8a26bacf0109bca9068a3986d3794ce83b0
Johanna Casado <johi.ceh@gmail.com>
2022-04-04 11:23:21 -0300

    Flag to save the plot
    A flag in bash was setted to indicate if the user want the plot or not.

--- sonoUno/sonify_bash.py
+++ sonoUno/sonify_bash.py
@@ -25,10 +25,6 @@ _math = PredefMathFunctions()
 _simplesound.reproductor.set_continuous()
 _simplesound.reproductor.set_waveform('celesta')
 _simplesound.reproductor.set_time_base(0.05)
-# Create an empty figure or plot to save it
-fig = plt.figure()
-# Defining the axes so that we can plot data into it.
-ax = plt.axes()
 # The argparse library is used to pass the path and extension where the data
 # files are located
 parser = argparse.ArgumentParser()
@@ -39,11 +35,16 @@ parser.add_argument("-t", "--file-type", type=str,
 # Receive the directory path from the arguments
 parser.add_argument("-d", "--directory", type=str,
                     help="Indicate a directory to process as batch.")
+# Indicate to save or not the plot
+parser.add_argument("-p", "--save-plot", type=bool,
+                    help="Indicate if you want to save the plot (False as default)",
+                    choices=[False,True])
 # Alocate the arguments in variables, if extension is empty, select txt as
 # default
 args = parser.parse_args()
 ext = args.file_type or 'txt'
 path = args.directory
+plot_flag = args.save_plot or False
 # Print a messege if path is not indicated by the user
 if not path:
     print('At least on intput must be stated.\nUse -h if you need help.')
@@ -52,6 +53,11 @@ if not path:
 extension = '*.' + ext
 # Initialize a counter to show a message during each loop
 i = 1
+if plot_flag:
+    # Create an empty figure or plot to save it
+    fig = plt.figure()
+    # Defining the axes so that we can plot data into it.
+    ax = plt.axes()
 # Loop to walk the directory and sonify each data file
 now = datetime.datetime.now()
 print(now.strftime('%Y-%m-%d_%H-%M-%S'))
@@ -69,21 +75,22 @@ for filename in glob.glob(os.path.join(path, extension)):
     y = data.loc[1:, 1]
     ynumpy = y.values.astype(np.float64)
     x, y, status = _math.normalize(xnumpy, ynumpy)
-    # Configure axis, plot the data and save it
-    # Erase the plot
-    ax.cla()
-    # First file of the column is setted as axis name
-    x_name = str(data.iloc[0,0])
-    ax.set_xlabel(x_name)
-    y_name = str(data.iloc[0,0])
-    ax.set_ylabel(y_name)
-    # Separate the name file from the path to set the plot title
-    head, tail = os.path.split(filename)
-    ax.set_title(tail)
-    ax.plot(xnumpy, ynumpy)
-    # Set the path to save the plot and save it
-    plot_path = path + '\\' + os.path.basename(filename) + '_plot.png'
-    fig.savefig(plot_path)
+    if plot_flag:
+        # Configure axis, plot the data and save it
+        # Erase the plot
+        ax.cla()
+        # First file of the column is setted as axis name
+        x_name = str(data.iloc[0,0])
+        ax.set_xlabel(x_name)
+        y_name = str(data.iloc[0,0])
+        ax.set_ylabel(y_name)
+        # Separate the name file from the path to set the plot title
+        head, tail = os.path.split(filename)
+        ax.set_title(tail)
+        ax.plot(xnumpy, ynumpy)
+        # Set the path to save the plot and save it
+        plot_path = path + '\\' + os.path.basename(filename) + '_plot.png'
+        fig.savefig(plot_path)
     # Save the sound
     wav_name = path + '\\' + os.path.basename(filename) + '_sound.wav'
     _simplesound.save_sound(wav_name, x, y)
